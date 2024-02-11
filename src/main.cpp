64acbcb7433f10e5b76720f3a39c06cfc408cf16
Brian Ortiz <bortiiz.bao@gmail.com>
2024-02-10 19:14:06 -0800

    testing nlohmann

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include "cstring"
+#include <nlohmann/json.hpp>
 
 int main()
 {
-    std::cout << "fck this" << std::endl;
+    std::fstream jsonFileStream("./DataStorage/UserFile.json");
+    nlohmann::json jsonData = nlohmann::json::parse(jsonFileStream);
 
-    return 0;
+    //std::ofstream jsonOut("./DataStorage/testFile.json");
+    //jsonOut << std::setw(4) << jsonData;
+
+    std::cout << "Thread Count: " << jsonData["thread_count"] << std::endl;
+    std::cout << "Server Host: " << jsonData["server"]["host"].get<std::string>() << std::endl;
+    std::cout << "Server Port: " << jsonData["server"]["port"].get<unsigned int>() << std::endl;
+
+    jsonData.find("thread_count");
+
+    std::cout << std::setw(4) << jsonData; // setw = pretty printing
 }
